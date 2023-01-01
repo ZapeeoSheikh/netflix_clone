87725d7ce876f8768f658f545a194a36c6e77ca2
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-01-01 22:20:32 +0500

    1st commit of the year

--- lib/custom/startup_container.dart
+++ lib/custom/startup_container.dart
@@ -4,12 +4,14 @@ import 'package:netflix_clone/routes/routes.dart';
 
 class StartUpContainer extends StatefulWidget {
   const StartUpContainer({Key? key}) : super(key: key);
+
   @override
   State<StartUpContainer> createState() => _StartUpContainerState();
 }
 
 class _StartUpContainerState extends State<StartUpContainer> {
   int index = 1;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -23,18 +25,19 @@ class _StartUpContainerState extends State<StartUpContainer> {
         height: 110,
         width: 120,
         decoration: BoxDecoration(
-            color: index == 1 ? Colors.red : Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // changes position of shadow
-              ),
-            ],
-            borderRadius: BorderRadius.circular(5),
-            image: DecorationImage(
-                image: AssetImage("img/me.jpeg"), fit: BoxFit.cover),),
+          color: index == 1 ? Colors.red : Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+          borderRadius: BorderRadius.circular(5),
+          image: DecorationImage(
+              image: AssetImage("img/me.jpeg"), fit: BoxFit.cover),
+        ),
       ),
     );
   }
